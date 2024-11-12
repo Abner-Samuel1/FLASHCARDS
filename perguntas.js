32fdec491f666373b607eb887d543acc43d7a463
Abner <138796703+Abner-Samuel1@users.noreply.github.com>
2024-11-12 10:37:32 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,27 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o maior país do mundo em área territorial?',
+    'O maior país em área territorial é a Russia.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Conhecimentos Gerais',
+    'Normalmente, quantos litros de sangue uma pessoa tem?',
+    'Entre 4 a 6 litros.'
+)
+
+criaCartao(
+    'Matematica',
+    'Quantas casas decimais tem o número pi?',
+    'O numero pi tem infinitas casa decimais.'
 )
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Ingles',
+    'O que a palavra legend significa em português?',
+    'Lenda.'
 ) 
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    '8 minutos.'
+)
